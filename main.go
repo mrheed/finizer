fb36b35e63af400358cc45f9006d961cc59deef8
root <root@TeamOs-PC.localdomain>
2021-04-05 11:15:13 +0530

    re indentation

--- main.go
+++ main.go
@@ -1,76 +1,76 @@
 package main
 
 import (
-	"errors"
-	"flag"
-	"fmt"
-	"os"
-	"time"
+  "errors"
+  "flag"
+  "fmt"
+  "os"
+  "time"
 
-	lib "github.com/syahidnurrohim/file_classifier/src"
+  lib "github.com/syahidnurrohim/file_classifier/src"
 )
 
 var (
-	move                bool
-	copy                bool
-	help                bool
-	maxStairs           int
-	startDir            string
-	customFormatPath    string
-	ignoreDefaultFormat bool
+  move                bool
+  copy                bool
+  help                bool
+  maxStairs           int
+  startDir            string
+  customFormatPath    string
+  ignoreDefaultFormat bool
 )
 
 func log(err error) {
-	fmt.Println(err.Error())
+  fmt.Println(err.Error())
 }
 
 func init() {
-	currentDirectory, err := os.Getwd()
-	if err != nil {
-		fmt.Println(err.Error())
-		os.Exit(0)
-	}
-	flag.BoolVar(&ignoreDefaultFormat, "ignoreDefaultFormat", false, lib.DefaultMimeHelper)
-	flag.StringVar(&customFormatPath, "pathToFormat", "", lib.CustomFormatPathHelper)
-	flag.IntVar(&maxStairs, "maxStairs", -1, lib.MaxStairsHelper)
-	flag.StringVar(&startDir, "begin", currentDirectory, lib.BeginHelper)
-	flag.BoolVar(&move, "move", false, lib.MoveHelper)
-	flag.BoolVar(&copy, "copy", false, lib.CopyHelper)
-	flag.BoolVar(&help, "help", false, lib.AllHelper)
-	flag.Parse()
+  currentDirectory, err := os.Getwd()
+  if err != nil {
+    fmt.Println(err.Error())
+    os.Exit(0)
+  }
+  flag.BoolVar(&ignoreDefaultFormat, "ignoreDefaultFormat", false, lib.DefaultMimeHelper)
+  flag.StringVar(&customFormatPath, "pathToFormat", "", lib.CustomFormatPathHelper)
+  flag.IntVar(&maxStairs, "maxStairs", -1, lib.MaxStairsHelper)
+  flag.StringVar(&startDir, "begin", currentDirectory, lib.BeginHelper)
+  flag.BoolVar(&move, "move", false, lib.MoveHelper)
+  flag.BoolVar(&copy, "copy", false, lib.CopyHelper)
+  flag.BoolVar(&help, "help", false, lib.AllHelper)
+  flag.Parse()
 }
 
 func main() {
-	switch true {
-	case help:
-		lib.CallHelpers()
-		return
-	case move && copy:
-		log(errors.New("parameters error: you cannot use --move argument alongside with --copy and otherwise"))
-		return
-	case !move && !copy:
-		log(errors.New("parameter error: you should put at least one argument from one of --move and --copy"))
-		return
-	case ignoreDefaultFormat && customFormatPath == "":
-		log(errors.New("parameters error: specify --pathToFormat directory in order to locate the custom format filtering rules"))
-		return
-	default:
-		fmt.Println("Start to investigating files, this perform may take a moment depends on the number of files...")
-		for range time.Tick(time.Second * 1) {
-			break
-		}
-	}
-	dir := lib.NewDir(startDir, customFormatPath, ignoreDefaultFormat, maxStairs)
-	moveable, err := dir.ReadDir(0)
-	if err != nil {
-		log(err)
-		return
-	}
-	lib.Sort(&moveable, 0, len(moveable)-1)
-	lib.PrepareController(moveable, move, copy).Start()
-	fmt.Println("Highest stairs", dir.HighestStairs)
-	fmt.Println("Total folder explored", dir.FolderExplored)
-	fmt.Println("Total file explored", dir.FileExplored)
-	fmt.Println("Total items", dir.TotalItems)
-	fmt.Println("Total skipped files", dir.FileSkipped)
+  switch true {
+  case help:
+    lib.CallHelpers()
+    return
+  case move && copy:
+    log(errors.New("parameters error: you cannot use --move argument alongside with --copy and otherwise"))
+    return
+  case !move && !copy:
+    log(errors.New("parameter error: you should put at least one argument from one of --move and --copy"))
+    return
+  case ignoreDefaultFormat && customFormatPath == "":
+    log(errors.New("parameters error: specify --pathToFormat directory in order to locate the custom format filtering rules"))
+    return
+  default:
+    fmt.Println("Start to investigating files, this perform may take a moment depends on the number of files...")
+    for range time.Tick(time.Second * 1) {
+      break
+    }
+  }
+  dir := lib.NewDir(startDir, customFormatPath, ignoreDefaultFormat, maxStairs)
+  moveable, err := dir.ReadDir(0)
+  if err != nil {
+    log(err)
+    return
+  }
+  lib.Sort(&moveable, 0, len(moveable)-1)
+  lib.PrepareController(moveable, move, copy).Start()
+  fmt.Println("Highest stairs", dir.HighestStairs)
+  fmt.Println("Total folder explored", dir.FolderExplored)
+  fmt.Println("Total file explored", dir.FileExplored)
+  fmt.Println("Total items", dir.TotalItems)
+  fmt.Println("Total skipped files", dir.FileSkipped)
 }
--- src/directory.go
+++ src/directory.go
@@ -1,113 +1,113 @@
 package lib
 
 import (
-	"io/ioutil"
-	"strings"
+  "io/ioutil"
+  "strings"
 )
 
 type Dir struct {
-	UpDir               string
-	PrevDir             string
-	CustomFormatPath    string
-	IgnoreDefaultFormat bool
-	HighestStairs       int
-	MaxStairs           int
-	FolderExplored      int
-	FileSkipped         int
-	FileExplored        int
-	TotalItems          int
+  UpDir               string
+  PrevDir             string
+  CustomFormatPath    string
+  IgnoreDefaultFormat bool
+  HighestStairs       int
+  MaxStairs           int
+  FolderExplored      int
+  FileSkipped         int
+  FileExplored        int
+  TotalItems          int
 }
 
 type Moveable struct {
-	FileName string
-	Source   string
-	Dest     string
-	Size     int64
-	StairsAt int
+  FileName string
+  Source   string
+  Dest     string
+  Size     int64
+  StairsAt int
 }
 
 func NewDir(UpDir string, CustomFormatPath string, IgnoreDefaultFormat bool, MaxStairs ...int) *Dir {
-	return &Dir{
-		UpDir:               UpDir,
-		CustomFormatPath:    CustomFormatPath,
-		IgnoreDefaultFormat: IgnoreDefaultFormat,
-		PrevDir:             "",
-		FolderExplored:      1,
-		TotalItems:          1,
-		MaxStairs:           MaxStairs[0],
-	}
+  return &Dir{
+    UpDir:               UpDir,
+    CustomFormatPath:    CustomFormatPath,
+    IgnoreDefaultFormat: IgnoreDefaultFormat,
+    PrevDir:             "",
+    FolderExplored:      1,
+    TotalItems:          1,
+    MaxStairs:           MaxStairs[0],
+  }
 }
 
 func (d *Dir) ReadDir(stair int) ([]Moveable, error) {
-	var result []Moveable
-	var currentDir string = d.PrevDir + "/" + d.UpDir
-	if d.PrevDir == "" {
-		currentDir = d.UpDir
-	}
-	files, err := ioutil.ReadDir(currentDir)
-	if err != nil {
-		return []Moveable{}, err
-	}
-	for _, file := range files {
-		d.TotalItems++
-		if d.HighestStairs < stair {
-			d.HighestStairs = stair
-		}
-		if file.IsDir() {
-			d.FolderExplored++
-			d.UpDir, d.PrevDir = file.Name(), currentDir
-			if (d.MaxStairs >= 0 && stair >= d.MaxStairs) || d.mimeDirIsExist(file.Name()) {
-				continue
-			}
-			moveable, err := d.ReadDir(stair + 1)
-			if err != nil {
-				return []Moveable{}, err
-			}
-			result = append(result, moveable...)
-		} else {
-			d.FileExplored++
-			fileInf, err := NewFile(file.Name(), file.Size(), d.CustomFormatPath, d.IgnoreDefaultFormat).Investigate()
-			if err != nil {
-				return []Moveable{}, err
-			}
-			if fileInf.Skip {
-				d.FileSkipped++
-				continue
-			}
-			targetDir := currentDir + "/" + fileInf.Folder + "/" + file.Name()
-			source := currentDir + "/" + file.Name()
-			result = append(result, Moveable{FileName: file.Name(), Source: trimPrefix(source), Dest: trimPrefix(targetDir), StairsAt: stair, Size: file.Size()})
-		}
-	}
-	return result, nil
+  var result []Moveable
+  var currentDir string = d.PrevDir + "/" + d.UpDir
+  if d.PrevDir == "" {
+    currentDir = d.UpDir
+  }
+  files, err := ioutil.ReadDir(currentDir)
+  if err != nil {
+    return []Moveable{}, err
+  }
+  for _, file := range files {
+    d.TotalItems++
+    if d.HighestStairs < stair {
+      d.HighestStairs = stair
+    }
+    if file.IsDir() {
+      d.FolderExplored++
+      d.UpDir, d.PrevDir = file.Name(), currentDir
+      if (d.MaxStairs >= 0 && stair >= d.MaxStairs) || d.mimeDirIsExist(file.Name()) {
+        continue
+      }
+      moveable, err := d.ReadDir(stair + 1)
+      if err != nil {
+        return []Moveable{}, err
+      }
+      result = append(result, moveable...)
+    } else {
+      d.FileExplored++
+      fileInf, err := NewFile(file.Name(), file.Size(), d.CustomFormatPath, d.IgnoreDefaultFormat).Investigate()
+      if err != nil {
+        return []Moveable{}, err
+      }
+      if fileInf.Skip {
+        d.FileSkipped++
+        continue
+      }
+      targetDir := currentDir + "/" + fileInf.Folder + "/" + file.Name()
+      source := currentDir + "/" + file.Name()
+      result = append(result, Moveable{FileName: file.Name(), Source: trimPrefix(source), Dest: trimPrefix(targetDir), StairsAt: stair, Size: file.Size()})
+    }
+  }
+  return result, nil
 }
 
 func (d *Dir) mimeDirIsExist(dirName string) bool {
-	if d.CustomFormatPath != "" {
-		customRules, err := UnmarshalCustomRules(d.CustomFormatPath)
-		if err != nil {
-			return false
-		}
-		for _, d := range customRules {
-			if d.Folder == dirName {
-				return true
-			}
-		}
-	}
-	if d.IgnoreDefaultFormat {
-		return false
-	}
-	if dirName == "Others" {
-		return true
-	}
-	for k, _ := range DefaultMime {
-		if k == dirName {
-			return true
-		}
-	}
-	return false
+  if d.CustomFormatPath != "" {
+    customRules, err := UnmarshalCustomRules(d.CustomFormatPath)
+    if err != nil {
+      return false
+    }
+    for _, d := range customRules {
+      if d.Folder == dirName {
+        return true
+      }
+    }
+  }
+  if d.IgnoreDefaultFormat {
+    return false
+  }
+  if dirName == "Others" {
+    return true
+  }
+  for k, _ := range DefaultMime {
+    if k == dirName {
+      return true
+    }
+  }
+  return false
 }
 
 func trimPrefix(s string) string {
-	return strings.TrimPrefix(s, "./")
+  return strings.TrimPrefix(s, "./")
 }
